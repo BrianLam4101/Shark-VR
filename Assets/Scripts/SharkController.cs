d4db25bc7f1d2aa0cfd25eea864696e864af0532
Brian Lam <BrianLam4101@gmail.com>
2019-02-16 16:55:23 -0800

    Movement and dash

--- Assets/Scripts/SharkController.cs
+++ Assets/Scripts/SharkController.cs
@@ -6,27 +6,46 @@ using UnityEngine.UI;
 public class SharkController : MonoBehaviour {
 
     private Camera mainCamera;
+    private new Rigidbody rigidbody;
     [SerializeField]
     private float avgTime = 0.5f;
     private float totalVel = 0;
     private float AvgVel {
-        get { return totalVel / runningCount.Count; }
+        get { return runningCount.Count > 0 ? totalVel / runningCount.Count : 0; }
     }
     private Queue<FrameRotation> runningCount = new Queue<FrameRotation>();
+    [SerializeField]
+    private float anglesPerUnit = 90;
+    private float speed;
+
+    [SerializeField]
+    private float DashCoolDown = 0.2f;
+    private float dashCoolDownTimer = 0;
+    private bool dashing = false;
 
     public Slider debugVelocity;
 
     void Start() {
         mainCamera = Camera.main;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update() {
         UpdateAverageVelocity();
         debugVelocity.value = AvgVel;
+
+        if (!dashing)
+            speed = AvgVel / anglesPerUnit;
+
+        if (dashCoolDownTimer <= 0 && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) {
+            StartCoroutine(Dash());
+            dashCoolDownTimer = DashCoolDown;
+        }
+        dashCoolDownTimer -= Time.deltaTime;
     }
 
     void FixedUpdate() {
-
+        rigidbody.velocity = mainCamera.transform.forward * speed;
     }
 
     private void UpdateAverageVelocity() {
@@ -37,6 +56,23 @@ public class SharkController : MonoBehaviour {
         float angVelMagnitude = OVRInput.GetLocalControllerAngularVelocity(activeController).magnitude * Mathf.Rad2Deg;
         totalVel += angVelMagnitude;
         runningCount.Enqueue(new FrameRotation(angVelMagnitude));
+    }
+
+    private IEnumerator Dash() {
+        dashing = true;
+        float initSpeed = speed;
+        speed *= 1.5f;
+        speed += 15;
+        yield return new WaitForSeconds(0.15f);
+        float velocity = 0;
+        float timer = 0.05f;
+        while (timer > 0) {
+            speed = Mathf.SmoothDamp(speed, initSpeed, ref velocity, 0.05f);
+            timer -= Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        speed = initSpeed;
+        dashing = false;
     }
 
     private class FrameRotation {
